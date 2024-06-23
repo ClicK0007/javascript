bbd0089802c18cc68114f66fa62ff6be5e798dd9
ClicK0007 <122447960+ClicK0007@users.noreply.github.com>
2024-06-23 14:30:32 +0000

    object done

--- 01_Basics/05_objects.js
+++ 01_Basics/05_objects.js
@@ -1,11 +1,21 @@
 // const tinderUser = new Object() // gives empty object but SINGLETON
 const tinderUser = {} // empty object literal
 
-tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
-tinderUser.isLoggedIn = false
+// tinderUser.id = "123abc"
+// tinderUser.name = "Sammy"
+// tinderUser.isLoggedIn = false
 
-console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
+// console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+
+// console.log(Object.keys(tinderUser)); // to get all the keys
+// console.log(Object.values(tinderUser)); // to get all the values
+// console.log(Object.entries(tinderUser)); // to get key + values
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // to check if there is property named isLoggedIn
+
+
+
 
 // const regularUser = {
 //     email: "some@gmail.com",
@@ -17,55 +27,45 @@ console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 //     }
 // }
 
-// // console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname); // hitesh
 
-// const obj1 = {1: "a", 2: "b"}
-// const obj2 = {3: "a", 4: "b"}
+
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = { obj1, obj2 } 
+// console.log(obj3); // Nested object problem
+
+
+// Can be solved by Object.assign(target, source) 
 // const obj4 = {5: "a", 6: "b"}
+// const obj5 = Object.assign(obj1, obj2, obj4) // obj1 becomes target so changes REFLECTED on obj1 also
+// console.log(obj5); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+// console.log(obj1); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
-// // const obj3 = { obj1, obj2 }
-// // const obj3 = Object.assign({}, obj1, obj2, obj4)
-
-// const obj3 = {...obj1, ...obj2}
-// // console.log(obj3);
-
-
-// const users = [
-//     {
-//         id: 1,
-//         email: "h@gmail.com"
-//     },
-//     {
-//         id: 1,
-//         email: "h@gmail.com"
-//     },
-//     {
-//         id: 1,
-//         email: "h@gmail.com"
-//     },
-// ]
-
-// users[1].email
-// // console.log(tinderUser);
-
-// // console.log(Object.keys(tinderUser));
-// // console.log(Object.values(tinderUser));
-// // console.log(Object.entries(tinderUser));
-
-// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// const obj6 = Object.assign({},obj1, obj2, obj4) // {} IS TARGET // RECOMMENDED to give empty object as target so no object can get changed
 
 
-// const course = {
-//     coursename: "js in hindi",
-//     price: "999",
-//     courseInstructor: "hitesh"
-// }
+// const obj3 = {...obj1, ...obj2} // By using spread operator
 
-// // course.courseInstructor
 
-// const {courseInstructor: instructor} = course
 
-// // console.log(courseInstructor);
+
+//-------Object Destructuring---------
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+
+const {courseInstructor} = course // courseInstructor is variable where value getting stored from course object
+// but key has to be ma
+
+console.log(courseInstructor);
 // console.log(instructor);
 
 // // {
