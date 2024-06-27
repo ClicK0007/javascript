f7703a9a2bb0008a412d4b32839ed77683574b8f
ClicK0007 <122447960+ClicK0007@users.noreply.github.com>
2024-06-27 21:24:49 +0000

    promise half complete

---
+++ 07_api/02_promise.js
@@ -0,0 +1,103 @@
+// Promise One
+const promiseOne = new Promise(function(resolve, reject){ // takes 2 arguments
+    // Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is compelete');
+        resolve() // VERY IMPORTANT TO CALL .then
+    }, 1000)
+})
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
+// Promise Two
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+// Promise Three
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"}) // Passing value to .then function
+    }, 1000)
+})
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+// Promise Four
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected")) // These is going to execute anyway
+
+
+// Promise Five
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+async function consumePromiseFive(){
+    try { // try catch is neccessary if there is chances of reject
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+// // Getting Data from API
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json() // response.json() will return promise
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+//getAllUsers()
+
+/// CONSOLE LOG THESE API DATA IS COMING FIRST
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
