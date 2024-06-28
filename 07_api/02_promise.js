9d7340020e77d575087e9ceeeaa93624bea1d3cc
ClicK0007 <122447960+ClicK0007@users.noreply.github.com>
2024-06-28 06:08:31 +0000

    Promises completed

--- 07_api/02_promise.js
+++ 07_api/02_promise.js
@@ -1,3 +1,9 @@
+/*
+Q. WHAT IF STATUS CODE IS 400 OR 404 WHERE WILL BE THE EXECUTION GO IN .then OR in .catch BLOCK?????????
+IN .then BLOCK SAME WITH ASYNC AWAIT TRY CATCH BLOCK
+The Fetch API considers these status codes as successful fetch operations from a network perspective, so the response will go to the .then block. However, you'll likely want to handle these status codes as errors, which is why it's common practice to check the response.ok property in the .then block and throw an error if it's false.
+*/
+
 // Promise One
 const promiseOne = new Promise(function(resolve, reject){ // takes 2 arguments
     // Do an async task
@@ -101,3 +107,35 @@ fetch('https://api.github.com/users/hiteshchoudhary')
 
 // promise.all
 // yes this is also available, kuch reading aap b kro.
+/*
+The Promise.all() static method takes an iterable of promises as input and returns a single Promise. This returned promise fulfills when all of the input's promises fulfill (including when an empty iterable is passed), with an array of the fulfillment values. It rejects when any of the input's promises rejects, with this first rejection reason.
+*/
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+// output: Array [3, 42, "foo"]
+
+/* 
+Return value
+A Promise that is:
+-   Already fulfilled, if the iterable passed is empty. 
+    const promises = []; // An empty array
+    Promise.all(promises)
+    .then(result => {
+        console.log('Promise is already fulfilled:', result); 
+        // Output: Promise is already fulfilled: []
+    })
+    .catch(error => {
+        console.error('This will not be called because the promise is already fulfilled.');
+    });
+
+-   Asynchronously fulfilled, when all the promises in the given iterable fulfill. The      fulfillment value is an array of fulfillment values, in the order of the PROMISES passed, regardless of COMPLETION ORDER. If the iterable passed is non-empty but contains no pending promises, the returned promise is still asynchronously (instead of synchronously) fulfilled.
+-    Asynchronously rejected, when any of the promises in the given iterable rejects. The rejection reason is the rejection reason of the FIRST promise that was rejected.
+*/
